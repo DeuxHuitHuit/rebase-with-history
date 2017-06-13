79e519d3cdf1227086efa51e4be0a79a139da500
Nicolas Brassard <nitriques@users.noreply.github.com>
2017-06-13 17:13:33 -0400

    Added 3 new switches: help, version, continue
    Help and version are not working right now (they need to be checked in
    the js file) but at least, when you ran into a conflict, the automated
    rebased and continue!

--- bin/rwh.sh
+++ bin/rwh.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-ED="$(dirname $0)/rwh.js ${@:2}";
+ED="$(dirname $0)/rwh.js";
 echo $ED;
 GED="$GIT_EDITOR";
-export GIT_EDITOR=$ED;
-git rebase -i $1
+
+for i in "$@"
+do
+case $i in
+	--continue)
+		shift # past argument=value
+		export GIT_EDITOR="${ED}";
+		git rebase --continue
+	;;
+	--version)
+		node $ED --version;
+		exit;
+	;;
+	--help)
+		node $ED --help;
+		exit;
+	;;
+	*)
+		export GIT_EDITOR="${ED} ${@:2}";
+		git rebase -i $1
+	;;
+esac
+done
+
 export GIT_EDITOR=$GED;
